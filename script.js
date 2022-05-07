be8735082fdf09122ffcb61b49caea1b955798d9
Tomasz <tomigunpl@icloud.com>
2022-05-07 17:28:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -41,17 +41,17 @@ document.addEventListener('keydown', function (e) {
 // Przycisk ze scrollem do 1 sekcji
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
-  console.log(s1coords);
+  // console.log(s1coords);
 
-  console.log(e.target.getBoundingClientRect());
+  // console.log(e.target.getBoundingClientRect());
 
-  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+  // console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
 
-  console.log(
-    'height/width viewport',
-    document.documentElement.clientHeight,
-    document.documentElement.clientWidth
-  );
+  // console.log(
+  //   'height/width viewport',
+  //   document.documentElement.clientHeight,
+  //   document.documentElement.clientWidth
+  // );
 
   section1.scrollIntoView({ behavior: 'smooth' });
 });
